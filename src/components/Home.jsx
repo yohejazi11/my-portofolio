250592309d126d900beaf61d59c038d560d33e63
yohejazi11 <yohejazi11@gmail.com>
2025-01-27 15:55:45 +0300

    add typical library

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { TypeAnimation } from 'react-type-animation';
 
 function Home() {
     const [scrollScale, setScrollScale] = useState(1);
@@ -66,9 +67,51 @@ function Home() {
 
             {/* النصوص */}
             <div className=" absolute flex flex-col gap-y-[1rem] top-[10vh] h-[85%] w-[90%] p-[20px] z-10 text-[6rem] font-semibold text-white">
-                <p className="border-t-[1px] border-white w-[fit-content] self-start px-[20px] text-border">HELLO</p>
-                <p className="text-right border-t-[1px] border-white w-[fit-content] self-end px-[20px] max-xl:text-[4rem] text-border">I AM YOUSEF HEJAZI</p>
-                <p className="border-t-[1px] border-white w-[fit-content] self-start px-[20px] max-xl:text-[3rem] max-sm:text-[1.5rem] text-border">FRONT-END DEVELOPER</p>
+                <p className="border-t-[1px] border-white w-[fit-content] self-start px-[20px] text-border">
+                    <TypeAnimation
+                        sequence={[
+                            'HELLO', // النص الأول
+                            20000,    // تأخير لمدة 1 ثانية بعد الكتابة
+                            '',      // حذف النص
+                            1000,    // تأخير قبل إعادة الكتابة
+                        ]}
+                        wrapper="span"
+                        cursor={false} // إذا أردت المؤشر، اجعلها true
+                        speed={1} // سرعة الكتابة
+                        repeat={1} // التكرار بشكل لا نهائي
+                        deletionSpeed={1} // سرعة الحذف
+                    />
+                </p>
+                <p className="text-right border-t-[1px] border-white w-[fit-content] self-end px-[20px] max-xl:text-[4rem] text-border">
+                <TypeAnimation
+                        sequence={[
+                            'I AM YOUSEF HEJAZI', // النص الأول
+                            20000,    // تأخير لمدة 1 ثانية بعد الكتابة
+                            '',      // حذف النص
+                            1000,    // تأخير قبل إعادة الكتابة
+                        ]}
+                        wrapper="span"
+                        cursor={false} // إذا أردت المؤشر، اجعلها true
+                        speed={2} // سرعة الكتابة
+                        repeat={0} // التكرار بشكل لا نهائي
+                        deletionSpeed={1} // سرعة الحذف
+                    />
+                    </p>
+                <p className="border-t-[1px] border-white w-[fit-content] self-start px-[20px] max-xl:text-[3rem] max-sm:text-[1.5rem] text-border">
+                <TypeAnimation
+                        sequence={[
+                            'FRONT-END DEVELOPER', // النص الأول
+                            20000,    // تأخير لمدة 1 ثانية بعد الكتابة
+                            '',      // حذف النص
+                            1000,    // تأخير قبل إعادة الكتابة
+                        ]}
+                        wrapper="span"
+                        cursor={true} // إذا أردت المؤشر، اجعلها true
+                        speed={3} // سرعة الكتابة
+                        repeat={0} // التكرار بشكل لا نهائي
+                        deletionSpeed={1} // سرعة الحذف
+                    />
+                    </p>
 
             </div>
         </div>
