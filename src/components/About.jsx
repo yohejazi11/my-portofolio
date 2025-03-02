e7e287a2385e0765a8575d9bc50ce6e847f5b069
yohejazi11 <yohejazi11@gmail.com>
2025-03-03 02:21:15 +0300

    edite cv link

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,7 @@ function About() {
             <div className="flex justify-center gap-x-[2rem] w-[100%] mt-[2rem]">
                 <a
                     className="w-[200px] h-[64px] max-sm:w-[150px] flex justify-center items-center rounded-[8px] bg-[#DD5746] text-[#8B322C] hover:bg-[#FFC470] hover:text-[#DD5746] hover:border-[#DD5746] hover:border-[2px] transition-all duration-300 text-[2rem] max-sm:text-[1.5rem] font-semibold text-center button-shadow"
-                    href="https://drive.google.com/file/d/1rVxpTN9DrnkQT7-GWWEANfI12h0zbo8F/view?usp=sharing"
+                    href="https://drive.google.com/file/d/19HvTKcF-L7EvsxVEoXk5xi_8DVOy2pIJ/view?usp=sharing"
                 >CV</a>
                 <a
                     className="w-[200px] h-[64px] max-sm:w-[150px]  flex justify-center items-center rounded-[8px] bg-[#FFC470] text-[#DD5746] hover:bg-[#DD5746] hover:text-[#8B322C] hover:border-[#8B322C] border-[#DD5746] border-[2px] transition-all duration-300 text-[2rem] max-sm:text-[1.5rem] font-semibold text-center button-shadow"
